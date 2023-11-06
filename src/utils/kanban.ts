2f7d3e41862df6a8c85f93e3ea7cab07d86f2ffa
gbbelloponce <personal.galo.bello@gmail.com>
2023-11-06 05:05:15 -0300

    Add style for each column

--- src/utils/kanban.ts
+++ src/utils/kanban.ts
@@ -6,4 +6,12 @@ const ColumnToTitle: Record<Columns, string> = {
   3: 'Done',
 }
 
+const ColumnToStyle: Record<Columns, string> = {
+  1: 'bg-red-300',
+  2: 'bg-yellow-300',
+  3: 'bg-green-300',
+}
+
 export const getTitleFromColumnType = (type: Columns) => ColumnToTitle[type]
+
+export const getStyleFromColumnType = (type: Columns) => ColumnToStyle[type]
