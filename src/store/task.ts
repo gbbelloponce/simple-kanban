314666484cbcebb8e7b320dffeaf1d8d35ae0c3f
gbbelloponce <personal.galo.bello@gmail.com>
2023-11-11 16:15:46 -0300

    Add tasks initial state

--- src/store/task.ts
+++ src/store/task.ts
@@ -1,6 +1,27 @@
 import { create } from 'zustand'
 
 export const useTaskStore = create<SK.TaskStore>((set) => ({
-  tasks: [],
+  tasks: [
+    {
+      description: 'A task',
+      id: '1',
+      column: 1,
+    },
+    {
+      description: 'A asdtask',
+      id: '5',
+      column: 1,
+    },
+    {
+      description: 'Another task',
+      id: '2',
+      column: 2,
+    },
+    {
+      description: 'Other task',
+      id: '3',
+      column: 3,
+    },
+  ],
   addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
 }))
