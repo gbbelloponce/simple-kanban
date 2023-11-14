e73ea67f2ffbf638a684845457368ea508503450
gbbelloponce <personal.galo.bello@gmail.com>
2023-11-13 22:54:14 -0300

    Fix input in systems dark mode

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -51,7 +51,7 @@ export const TaskForm: FC = () => {
               Description
             </label>
             <input
-              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              className='shadow appearance-none border rounded w-full py-2 px-3 bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
               id='description'
               title='description'
               type='text'
